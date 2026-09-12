2fd098548c0f5b3d5faf1a3328358b012499539d
GolovinEG <e_golovin@list.ru>
2026-09-12 13:04:07 +0300

    Заполнение в стриме

---
+++ src/data/ProviderStream.java
@@ -0,0 +1,74 @@
+package data;
+
+import model.Car;
+
+import java.util.Optional;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class ProviderStream {
+    private static final String MENU_STR = """
+            \n--- Заполнение массива данными ---
+            1. Рандом
+            2. Из файла
+            3. Вручную
+            Выберите вариант: """;
+
+    private static Stream<List<Car>> getDataList(Scanner scanner) {
+        final Supplier<List<Car>> readData = () -> {
+            while (true) {
+                try {
+                    final int selection;
+                    final List<Car> cars;
+                    System.out.print(MENU_STR);
+                    selection = scanner.nextInt();
+                    switch (selection) {
+                        case 1:
+                            System.out.print("Введите количество машин: ");
+                            final int count = scanner.nextInt();
+                            if (count <= 0) {
+                                System.out.println("Количество должно быть больше 0!");
+                                return List.of();
+                            } else {
+                                cars = DataProvider.generateRandom(count);
+                                System.out.println("Добавлено " + cars.size() + " машин.");
+                                return cars;
+                            }
+                        case 2:
+                            System.out.print("Введите имя файла: ");
+                            final String fileName = scanner.next();
+                            cars = DataProvider.readFromFile(fileName, scanner);
+                            System.out.println("Загружено " + cars.size() + " машин из файла.");
+                            return cars;
+                        case 3:
+                            cars = DataProvider.readFromConsole(scanner);
+                            System.out.println("Добавлено " + cars.size() + " машин.");
+                            return cars;
+                        default:
+                            System.out.println("Ошибка: нет данного выбора!");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка: введите число!");
+                }
+            }
+        };
+
+        return Stream.of(readData)
+                .map(Supplier::get);
+    }
+
+    public static void addToList(List<Car> carList, Scanner scanner) {
+        getDataList(scanner).flatMap(List::stream).forEach(carList::add);
+    }
+
+    public static void overwriteList(List<Car> carList, Scanner scanner) {
+        getDataList(scanner).findAny()
+                .ifPresent(newCarList -> {
+                    carList.removeAll(carList);
+                    carList.addAll(newCarList);
+                });
+    }
+}
---
+++ src/data/ProviderStreamTest.java
@@ -0,0 +1,57 @@
+package data;
+
+import model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProviderStreamTest {
+    private static final String FILENAME = "sorted_cars.json";
+    private static final int CAR_COUNT = 3;
+    private static final int POWER = 800;
+    private static final String MODEL = "TEST CAR";
+    private static final int YEAR = 1999;
+    private static final String DATA = String.format("%d\n%s\n%d\n", POWER, MODEL, YEAR);
+    private static final String FILL_FILE = String.format("2\n%s\n1\n", FILENAME);
+    private static final String FILL_CONSOLE = String.format("3\n%d\n%s%s%s", CAR_COUNT, DATA, DATA, DATA);
+    private static final String FILL_RANDOM = String.format("1\n%d\n", CAR_COUNT);
+    private static final String TEST_STR = String.format("%s%s%s", FILL_FILE, FILL_CONSOLE, FILL_RANDOM);
+
+    private static int testCount = 0;
+
+    private static void test(int count, int target) {
+        testCount++;
+        if (count != target) {
+            throw new RuntimeException(String.format("Test %d failed\n", testCount));
+        }
+    }
+
+    private static void test(List<Car> carList, List<Car> targetList) {
+        testCount++;
+        final int size = carList.size();
+        if (size != targetList.size()) {
+            throw new RuntimeException(String.format("Test %d failed\n", testCount));
+        }
+        for (int i = 0; i < size; i++) {
+            if (!carList.get(i).equals(targetList.get(i))) {
+                throw new RuntimeException(String.format("Test %d failed\n", testCount));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running ProviderStream tests.");
+        final List<Car> carList = new ArrayList<>();
+        final List<Car> testList = DataProvider.readFromFile(FILENAME, new Scanner("1\n"));
+        final Scanner scanner = new Scanner(String.format(TEST_STR));
+        ProviderStream.addToList(carList, scanner);
+        test(carList.size(), testList.size());
+        test(carList, testList);
+        ProviderStream.overwriteList(carList, scanner);
+        test(carList.size(), CAR_COUNT);
+        ProviderStream.addToList(carList, scanner);
+        test(carList.size(), CAR_COUNT * 2);
+        System.out.println("All tests complete.");
+    }
+}
