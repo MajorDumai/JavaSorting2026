2957ef4050d059051eecb38a905a19bcd50f822e
Yancev Mikhail <al888a@mail.ru>
2026-09-12 19:36:08 +0300

    merged branch with VladimirD

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -201,7 +201,7 @@ public class Main {
             return;
         }
 
-        System.out.println("\n=== СПИСОК МАШИН ===");
+        System.out.println("\nСПИСОК МАШИН");
         for (int i = 0; i < cars.size(); i++) {
             System.out.println((i + 1) + ". " + cars.get(i));
         }
